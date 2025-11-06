e9043bfc372c8ab95080f5aada15dd03fc4d7a5a
ivanpoon23 <145502986+ivanpoon23@users.noreply.github.com>
2025-11-05 21:34:41 -0600

    .

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { HashRouter } from 'react-router-dom'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { HashRouter } from "react-router-dom"; // <-- use HashRouter
+import App from "./App";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
     <HashRouter>
       <App />
     </HashRouter>
-  </StrictMode>,
-)
+  </React.StrictMode>
+);
